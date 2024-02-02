3c86a5642776e9cd7405d661473e1d0497f44420
jos3376 <jr3376hotmail.com>
2024-02-01 23:48:52 -0300

    subo modificaciones realizadas

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
+const path = require('path')
+const port = 3030
+
 const app = express();
-const path = require("path");
-const port = 3030;
 
 app.use(express.static('public'))
 
-app.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
-  });
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
 
-
-app.listen(port,()=>{console.log(`http://localhost:${port}`)})
+app.listen(port,() =>console.log(`http://localhost:${port}`))
