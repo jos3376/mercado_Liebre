14f689944e7cc7431272d75da38d6b23dcfac243
jos3376 <jr3376hotmail.com>
2024-02-14 10:57:54 -0300

    subo modificaciones

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.use(express.static('public'))
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
-
+app.get('/home', (req, res) =>{
+    res.redirect('/');
+})
+app.get('/registro', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+app.get('/ingresa', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
 app.listen(port,() =>console.log(`http://localhost:${port}`))
